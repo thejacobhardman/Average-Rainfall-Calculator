9e8b6442af3b98c94dadfe435d96f55cc4ca1eb9
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-12 13:49:58 -0600

    format code and fix arithmetic overflow error

--- Average Rainfall Calculator/Average Rainfall Calculator.cpp
+++ Average Rainfall Calculator/Average Rainfall Calculator.cpp
@@ -4,20 +4,22 @@
 // 9/12/19
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+	cout << fixed << setprecision(2);
 	string monthNum[3] = { "1st", "2nd", "3rd" };
 	string months[3] = {};
-	int rainContainer[3] = {};
+	double rainContainer[3] = {};
 
    for (int i = 0; i < 3; i++) {
 	   string month;
 	   cout << "Please enter the name of the " << monthNum[i] << " month: ";
 	   cin >> month;
 
-	   int rainAmount;
+	   double rainAmount;
 	   cout << "Enter the rainfall for " << month << ": ";
 	   cin >> rainAmount;
 
@@ -25,6 +27,6 @@ int main()
 	   rainContainer[i] = rainAmount;
    }
 
-   float averageRainfall = (rainContainer[0] + rainContainer[1] + rainContainer[2]) / 3.0;
-   cout << "The average rainfall for " << months[0] << ", " << months[1] << ", and " << months[2] << " is " << averageRainfall << " inches.";
+   double averageRainfall = (rainContainer[0] + rainContainer[1] + rainContainer[2]) / 3.00;
+   cout << "\nThe average rainfall for " << months[0] << ", " << months[1] << ", and " << months[2] << " is " << averageRainfall << " inches.\n";
 }
